8c93f43de9e2db680c4d93ea893acf42f7592243
Rezwana Sultana <rezwana901@gmail.com>
2022-05-23 23:00:37 +0600

    v.1

---
+++ main.py
@@ -0,0 +1,285 @@
+print("Rezwana Sultana\n")
+
+#1. Make a Do or DIE game of your own! For help, see the example at the given Github link.
+print('''Ans1
+
+
+
+                                .,od88888888888bo,.
+                            .d88888888888888888888888b.
+                         .d88888888888888888888888888888b.
+                       .d888888888888888888888888888888888b.
+                     .d8888888888888888888888888888888888888b.
+                    d88888888888888888888888888888888888888888b
+                   d8888888888888888888888888888888888888888888b
+                  d888888888888888888888888888888888888888888888
+                  8888888888888888888888888888888888888888888888
+                  8888888888888888888888888888888888888888888888
+                  8888888888888888888888888888888888888888888888
+                  Y88888888888888888888888888888888888888888888P
+                  "8888888888P'   "Y8888888888P"    "Y888888888"
+                   88888888P        Y88888888P        Y88888888
+                   Y8888888          ]888888P          8888888P
+                    Y888888          d888888b          888888P
+                     Y88888b        d88888888b        d88888P
+                      Y888888b.   .d88888888888b.   .d888888
+                       Y8888888888888888P Y8888888888888888
+                        888888888888888P   Y88888888888888
+                        "8888888888888[     ]888888888888"
+                           "Y888888888888888888888888P"
+                                "Y88888888888888P"
+                             888b  Y8888888888P  d888
+                             "888b              d888"
+                              Y888bo.        .od888P
+                               Y888888888888888888P
+                                "Y88888888888888P"
+                                  "Y8888888888P"
+          d8888bo.                  "Y888888P"                  .od888b
+         888888888bo.                  """"                  .od8888888
+         "88888888888b.                                   .od888888888[
+         d8888888888888bo.                              .od888888888888
+       d88888888888888888888bo.                     .od8888888888888888b
+       ]888888888888888888888888bo.            .od8888888888888888888888b=
+       888888888P" "Y888888888888888bo.     .od88888888888888P" "Y888888P=
+        Y8888P"           "Y888888888888bd888888888888P"            "Y8P
+          ""                   "Y8888888888888888P"
+                                 .od8888888888bo.
+                             .od888888888888888888bo.
+                         .od8888888888P"  "Y8888888888bo.
+                      .od8888888888P"        "Y8888888888bo.
+                  .od88888888888P"              "Y88888888888bo.
+        .od888888888888888888P"                    "Y8888888888888888bo.
+       Y8888888888888888888P"                         "Y8888888888888888b=
+       888888888888888888P"                            "Y8888888888888888=
+        "Y888888888888888       __DO OR DIE GAME__         "Y88888888888888P=
+             ""Y8888888P                                  "Y888888P"
+                "Y8888P                                     Y888P"
+
+
+''')
+
+print("\nWelcome to DO or DIE Game Moana!.\nYour mission is to place the Heart of Te Fiti and Save the Mother Nature!.\n")
+print('''
+                                
+       ⣠⣴⣾⣿⣿⣷⣦⣄⠀⠀⠀⠀⠀⠀
+⠀⠀⢀⣠⣾⣿⣏⠀⠀  ⠉⠉⠉⢿⡆⠀⠀⠀⠀⠀
+⠀⠀⣼⣿⠁⠛⠤⣭⠙⠀⠀⣒ ⢺⣿⣄⠀⠀⠀⠀
+⢀⣴⣿⣿⣷⡄⠀⠀⠀⠀⠀⠉⣹⣿⣿⡀⠀⠀⠀
+⠞⢹⣿⣿⣿⣿⡦⠀⠘⠓⢀⣴⣿⣿⣿⣿⡆⠀⠀
+⠀⣼⣿⡿⠟⠛⠁⢀⣀⠀⠙⠛⠿⣿⣿⣿⣁⠀⠀
+⠸⠟⠉⠀⠀⠀  ⠀⠀⠀⠈⠻⣿⣯⡙⡆
+⠀⠀⠀⠀⠀⠀  ⠀⠀⠀⠀⠀⠈⠻⡇⠁
+''')
+
+input("LEVEL ONE (Press ENTER)")
+print("Moana! You are the chief of the island MOTUNUI. Your island is at stake. Mother nature is being destroyed for an unknown reason. \nYou are the only hope for the men of MOTUNUI. You need to stay beside them. But deep inside the Ocean is calling you for a mysterious reason! \nListen to your heart Moana! \nType 'O' to go to the Ocean. \nType 'M' to stay MOTUNUI.")
+
+decide = input("YOUR DECISION: ").lower()
+print(decide)
+
+if decide == "m":
+    print("Okay Moana! I wish you lead your men well. Good luck for your future. \n~~GAME OVER~~")
+elif decide == "o":
+    print('''You are such a brave soul! The Ocean is calling you. Mighty ocean is giving you the lost heart of Te Fiti. Te Fiti has been turned into Te Ka after losing her heart. Since then Te Ka is in the mission of ruining the mother nature. Your have to place the heart in the chest of Te Ka and turn this devil into Te Fiti, the Goddess of Nature again with the help of the Demigod Maui. \nFollow the Celestial Hook to the North of the sky and find Maui. \nBut this is morning. You cant see stars! Which direction will you go? Right or Left?
+    
+    ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣀⣀⣀⣀⣀⣀⣀⣀⣀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⢀⣠⣴⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣶⣦⣄⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⣠⣶⣿⣿⣿⣿⣿⣿⠿⠿⠿⠿⠿⠿⢿⣿⣿⣿⣿⣿⣿⣿⣷⣄⠀⠀⠀⠀⠀
+⠀⠀⢀⣾⣿⡿⠟⠋⠉⠁⠀⠀⠀⠀⠀⠀⣀⠀⠀⠀⠀⠉⠙⠿⣿⣿⣿⣿⣷⣄⠀⠀⠀
+⠀⢠⣿⣿⠟⠀⠀⠀⢀⣠⣤⣶⣾⣿⣿⣿⣿⣿⣿⣿⣶⣄⠀⠀⠈⢿⣿⣿⣿⣿⣷⡀⠀
+⠀⣿⣿⠋⠀⠀⣠⣶⣿⣿⠿⠛⠉⠁⠀⠀⠉⠉⠛⢿⣿⣿⣷⠀⠀⠈⢿⣿⣿⣿⣿⣷⡀
+⢸⣿⠇⠀⣠⣾⣿⣿⡟⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⣿⣿⡇⠀⠀⠘⣿⣿⣿⣿⣿⣇
+⢸⡏⢠⣾⣿⣿⣿⣿⠃⠀⠀⠀⣠⣶⣾⣶⣶⣦⡀⠀⠀⢻⣿⣿⠀⠀⠀⢿⣿⣿⣿⣿⣿
+⠈⣰⣿⣿⣿⣿⣿⠏⠀⠀⠀⢸⣿⣿⣿⣿⣿⣿⣿⣦⣀⣿⣿⡏⠀⠀⠀⣾⣿⣿⣿⣿⡏
+⢰⣿⣿⣿⣿⣿⣿⠀⠀⠀⠀⣿⣿⣿⡟⠛⠛⠻⠿⣿⣿⣿⠟⠁⠀⠀⢠⣿⣿⣿⣿⣿⠁
+⣾⣿⣿⣿⣿⣿⣿⡀⠀⠀⠀⣿⣿⣿⡇⠀⠀⠀⠀⠀⠁⠀⠀⠀⠀⣠⣿⣿⣿⣿⡿⠁⠀
+⢿⣿⣿⣿⣿⣿⣿⣧⠀⠀⠀⢹⣿⣿⣷⣤⣀⡀⠀⢀⣀⣀⣠⣤⣾⣿⣿⣿⠿⠋⠀⠀⠀
+⠘⣿⣿⣿⣿⣿⣿⣿⣧⡀⠀⠀⠉⠻⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠟⠋⠁⣀⠀⠀⠀⠀
+⠀⠹⣿⣿⣿⣿⣿⣿⣿⣷⣄⠀⠀⠀⠀⠀⠉⠙⠛⠛⠛⠛⠋⠉⣀⣤⣶⡿⠃⠀⠀⠀⠀
+⠀⠀⠈⠻⣿⣿⣿⣿⣿⣿⣿⣷⣄⣀⡀⠀⠀⢀⣀⣀⣤⣴⣾⣿⣿⡿⠋⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠈⠙⠻⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠿⠛⠁⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠈⠉⠛⠛⠛⠿⠿⠿⠟⠛⠛⠋⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+\nType 'R' to go Right. Type 'L' to go Left.''')
+    go = input("Which direction?\n").lower()
+    if go == "r":
+        print('''
+                           v  ~.      v
+          v           /|
+                     / //          v bzzzzz
+              v     /__|__        / 
+                  \--------/\/ \/ \www
+~~~~~~~~~~~~~~~~~~~`~~~~~~'~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+OH CRAP! A shoal of fishes attacked your boat! Your boat is sinking! Swim back to your home Moana :( \n Better luck next time. \n~~GAME OVER~~''')
+    elif go == "l":
+        print('''
+                 /***\**\     
+                /**/  \**\ 
+               /*/    /**/
+        ⠀⠀⠀⠀⠀⠀*⠀⠀ ⠀/**/⠀
+                    /***/  
+        /**************/⠀⠀⠀⠀⠀
+        That's the Celestial Hook! Sail your boat towards the hook. You will find an island. That's where Maui lives.''')
+        input("CONGRATULATIONS! \nYou passed LEVEL ONE! (Press ENTER to reach LEVEL TWO)")
+        print("Moana! Maui is suspecting you as an enemy. Convince him. You have two plans to convince. Type 'A' to follow Plan-A. Type 'B' to follow Plan-B.")
+
+        plan = input("YOUR PLAN: ").lower()
+        if plan == "a":
+            print('''Bribe your chicken 'HeiHei' to Maui:
+        ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⢠⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⣄⣿⣶⣿⣿⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢲⣾⣿⣿⣿⣿⠿⢇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠻⣿⣿⣿⠁⠀⡄⢡⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢧⢸⣻⣦⣤⣴⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣀⣠⣀⣀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣽⡟⣿⡏⠁⡈⠀⠀⠀⠀⠀⠀⣠⣴⣿⣿⠿⠿⠿⢿⣿⣆⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⠃⡟⠁⠀⡇⠀⠀⠀⠀⢀⣾⣿⡿⠋⠀⠀⠀⠀⠀⠀⠉⠳⡀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⡜⠀⠀⢀⣧⠀⠀⠀⢀⣼⣿⣿⣶⣿⠿⢿⣷⡀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀     ⠀⠀⠀⠀⠀⠀⡎⠀⡀⣼⣾⣿⣦⠀⠀⢸⣿⣿⣿⣭⡀⠀⠀⠻⣷⣄⡀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀           ⠀⢱⣆⣿⣿⣿⣿⣿⣦⣤⣾⣿⣿⣿⣿⣿⡄⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀          ⠀⠀⠀⠈⠛⢿⣿⣿⣿⣿⣿⣿⡿⣿⣿⣿⡿⢯⠉⠀⠀⠀⠀⠀⠀⠀⠀
+    ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀         ⠀⠀⠀⠀⠀⠹⣿⣿⣿⣿⡿⠀⠁⠏⠏⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀                  ⠀⠀⠀⠀⠀⠹⣿⠇⢿⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀                       ⠀⠀⠀⠀⠀⠀⣷⠀⠘⡇⠀⠀⠀⠀⠀⠀⠀⠀
+                                          ⡠⣹⣀⠂⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀
+''')
+            input("press ENTER")
+            print('''OOPS! Maui took your chicken. Locked you up in a cave and hijacked your boat! He ditched you!\nYou will die being locked up in this cave for days!"
+        _..--""---.
+          /           ".
+          `            l
+          |'._  ,._ l/"\ 
+          |  _J<__/.v._/
+           \( ,~._,,,,-)
+            `-\ ' \`,,j|
+               \_,____J
+          .--.__)--(__.--.
+         /  `-----..--'. j
+         '.- '`--` `--' \/  
+        //  '`---'`  `-' \/
+       //   '`----'`.-.-' \/
+     _//     `--- -'   \ | \________
+    |  |         ) (      `.__.---- -'/
+     \         \`-(               74///\ 
+     ||       _  /`-(               l|//7__
+     |l    ('  `-)-/_.--.          f''` -.-"|
+     |\     l\_  `-'    .'         |     |  |
+     llJ   _ _)J--._.-('           |     |  l
+     |||( ( '_)_  .l   ". _    ..__I     |  L
+     ^\||`'   "   '"-. " )''`'---'     L.-'`-.._
+          \ |           ) /.              ``'`-.._``-.
+          l l          / / |                      |''|
+           " \        / /   "-..__                |  |
+           | |       / /          1       ,- t-...J_.'
+           | |      / /           |       |  |
+           J  \  /"  (            l       |  |
+           | ().'`-()/            |       |  |
+          _.-"_.____/             l       l.-l
+      _.-"_.+"|                  /        \.  \ 
+/"\.-"_.-"  | |                 /          \   \ 
+\_   "      | |                1            | `'|
+  |ll       | |                |            i   |
+    \       |-\               \j ..          L,,'. `/
+ ____\     ( .-\           .--'    ``--../..'      '-..
+   `````///// .....--
+   \n~~GAME OVER~~''')
+        elif plan == "b":
+            print('''Deal with him that you will help him to get back his Lost Magical Hook in return for his help.
+    ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣤⣤⣤⣤⣄⣀⡀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⣾⣿⣿⠛⠛⠻⢿⣿⣿⣿⡶⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠜⠋⠉⠉⠉⠁⠀⠀⠀⣿⣿⣿⣿⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣀⣴⣶⣦⣤⣄⡀⠀⠀⠀⠀⠀⢀⣿⣿⣿⣿⠂
+⠀⠀⠀⠀⠀⢀⣠⣤⣀⣀⣠⣿⣿⣿⣿⣿⣿⣿⣿⣿⣤⡄⠀⣀⣤⣾⣿⣿⣿⡟⠀
+⢀⣀⣀⣤⣴⣿⣿⣿⣿⡿⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠿⠋⠀⠀
+⠹⠿⠿⠿⠿⣿⣿⣿⣿⣷⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣯⠉⠉⠁⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⡀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠈⠛⠛⠻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡆⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠋⣻⣿⣿⣿⣿⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⢐⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⣰⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠿⠋⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠛⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠟⠃⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⢹⢿⣿⣿⣿⣿⠿⠿⠿⣿⣿⣿⣿⣧⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⣠⣾⣿⣿⣿⡇⠀⠀⠀⠘⣿⣿⣿⣿⣄⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠉⠉⠉⠉⠉⠀⠀⠀⠀⠀⠉⠛⠛⠉⠉⠀⠀⠀⠀⠀⠀⠀⠀''')
+            print("Woohoo! Maui agreed to help you Moana! \nAs you promised... Find his Hook in the next LEVEL!")
+            input("CONGRATULATIONS! \nYou passed LEVEL TWO! (Press ENTER to reach to the FINAL LEVEL)")
+
+            print('''
+  /|  |\            /|  |\ 
+  /|  |\            /|  |\ 
+ / |  | \          / |  | \             *
+ | |  | |          | |  | |
+ \  \/  /  __  __  \  \/  /
+  \    /  / /  \ \  \    /
+   \  /   \ \__/ /   \  /       **
+   \  /   /      \   \  /
+  _ \ \__/ O    O \__/ / _
+  \/ \___          ___/ //
+_  \/___/  ______  \___//  _
+\/  ----(          )----  //
+ \/_____( ________ )_____//
+  ~-----(          )-----~ _
+   _____( ________ )_____  \/                   *
+  /,----(          )----  _//                  * *
+ //     (  ______  )     /  /
+ ~       \        /      \  /
+          \  __  /       / /
+           \    /       / /
+            \   \      / /
+             \   ~----~ /       *
+              \________/
+\nThis is Shiny Crab. His name is Tamatoa. Tamatoa kept Maui's Hook hidden. Fight him and win the Hook or Go back.''')
+            battle = input("Press 'F' to fight Tamatoa")
+            print("You won the fight. Tamatoa hands you over Maui's Hook. \nNow go to Te Ka and place the Heart. Be Brave! Type 'X' to attack. Type 'Y' to defend.")
+            war = input("How do you want to fight? ").lower()
+            if war == "x":
+                print('''Te Ka kills you and your fellow the Demigod Maui. You both tried hard. But you were unlucky. Better luck next time.
+                                            ,-.
+       ___,---.__          /'|`\          __,---,___
+    ,-'    \`    `-.____,-'  |  `-.____,-'    //    `-.
+  ,'        |           ~'\     /`~           |        `.
+ /      ___//              `. ,'          ,  , \___      \ 
+|    ,-'   `-.__   _         |        ,    __,-'   `-.    |
+|   /          /\_  `   .    |    ,      _/\          \   |
+\  |           \ \`-.___ \   |   / ___,-'/ /           |  /
+ \  \           | `._   `\  |  //'   _,' |            /  /
+  `-.\         /'  _ `---'' , . ``---' _  `\         /,-'
+     ``       /     \    ,='/ \`=.    /     \       ''
+             |__   /|\_,--.,-.--,--._/|\   __|
+             /  `./  \`\ |  |  | /,//' \,'  \ 
+Te Ka wins!/   /     ||--+--|--+-/-|     \   \ 
+           |   |     /'\_\_\ | /_/_/`\     |   |
+            \   \__, \_     `~'     _/ .__/   /
+             `-._,-'   `-._______,-'   `-._,-'
+\n~~~GAME OVER~~~''')
+            elif war == "y":
+                print('''CONGRATULATIONS! You have successfully stored the Heart of Te Fiti. You saved the Mother Nature!
+    ⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣤⣴⣶⣿⣿⣿⣿⣷⣶⣤⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⣠⣾⣿⣿⣿⠟⠁⠀⠀⠈⠙⠛⠿⠛⠛⠛⢿⣿⣧⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⣠⣴⣾⣿⣿⣿⣿⢃⡾⢀⣀⠀⠀⠀⠀⠀⠀⠀⠀⠸⣿⣿⡆⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⢸⣿⣿⣿⠃⠈⢿⡏⠈⠘⠛⠛⠻⠷⠀⠀⠀⢠⣤⣤⣤⣿⣿⣷⡀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⣸⣿⣿⣿⡀⠀⠈⠁⠚⠛⣿⣿⠆⠀⠀⠀⠀⢈⣉⣤⡈⣿⣿⣿⣿⣄⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⣠⣿⣿⣿⣿⣷⣤⣀⠀⠀⠀⠈⠁⠀⠀⠀⠀⠀⠘⠟⠉⠙⣿⣿⣿⣿⡿⠀⠀⠀⠀⠀⠀⠀
+⠀⣠⣾⣿⣿⣿⣿⣿⣿⣿⣿⡄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢰⣿⣿⣿⣿⣷⣄⠀⠀⠀⠀⠀⠀
+⢸⡟⢡⣿⣿⣿⣿⣿⣿⣿⣿⣿⣄⠀⠀⠀⠠⢤⣤⣤⠤⠀⠀⣠⣿⣿⣿⣿⣿⣿⣿⣷⡄⠀⠀⠀⠀
+⢻⠀⢸⣿⣿⣿⣿⣿⣿⣿⣿⣿⡟⠳⢄⡀⠀⠀⠀⠀⢀⣠⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠀⠀⠀⠀
+⠈⠀⠈⣿⣿⣿⣿⣿⣿⣿⣿⣿⠇⠀⠀⠀⠀⠀⠀⠐⠿⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡏⠀⠀⠀⠀
+⠀⢀⣼⣿⣿⣿⣿⠟⠛⠉⠁⠀⠀⠀⠀⣀⣀⣀⡀⠀⠀⠀⠀⠈⠉⠻⢿⣿⣿⣿⣿⣿⣧⣄⣀⠀⠀
+⢠⣿⣿⣿⠿⠋⠀⠀⠀⠀⠀⠀⠀⢰⢋⡭⠽⢯⡛⣷⡄⠀⠀⠀⠀⠀⠀⠙⢿⣿⣿⣿⣆⡙⠻⣷⣄
+⠘⠟⠋⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⣼⡟⢠⡶⠶⠇⣹⡇⠀⠀⠀⠀⠀⠀⠀⠀⠙⣿⣿⣿⣿⣦⠈⢿
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢻⣿⣌⠓⢒⣺⠏⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠻⣿⣿⣿⠀⠘
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠉⠉⠉⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠻⣿⠀⠀
+ \nYOU WON!!!''')
+
+
+
+ans2 = input("Press ENTER to EXIT from the GAME.\n")
+#2. yearly_temp = [[2016, 2017, 2018, 2019, 2020, 2021],[28, 30, 27.5, 30.5, 31, 29.9]]
+#Find the average temperature for the last six years (2015-2021), Find the maximum temperature and its year.
+print("Ans2")
+yearly_temp = [[2016, 2017, 2018, 2019, 2020, 2021],[28, 30, 27.5, 30.5, 31, 29.9]]
+n = len(yearly_temp[1])
+avg_temp = sum(yearly_temp[1])/n
+print("Average temperature is", round(avg_temp), "oC.")
+max_temp = max(yearly_temp[1])
+num = yearly_temp[1].index(max_temp)
+print("Maximum temperature is", max_temp, "oC and its year is ",yearly_temp[0][num],'.')
